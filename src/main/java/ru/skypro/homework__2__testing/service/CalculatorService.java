9728f343b248a40a20f545863214f1b9030bc44d
am999 <am9999072080@gmail.com>
2023-05-29 21:34:18 +0300

    Commit 1

--- src/main/java/ru/skypro/homework__2__testing/service/CalculatorService.java
+++ src/main/java/ru/skypro/homework__2__testing/service/CalculatorService.java
@@ -1,5 +1,9 @@
 package ru.skypro.homework__2__testing.service;
 
 public interface CalculatorService {
+    int sum(int num1, int num2);
+    int subtract(int num1, int num2);
+    int multiply(int num1, int num2);
+    int divide(int num1, int num2);
 
 }
--- src/test/java/ru/skypro/homework__2__testing/Homework2TestingApplicationTests.java
+++ src/test/java/ru/skypro/homework__2__testing/Homework2TestingApplicationTests.java
@@ -2,10 +2,12 @@ package ru.skypro.homework__2__testing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.skypro.homework__2__testing.service.CalculatorServiceImpl;
+
 
 @SpringBootTest
 class Homework2TestingApplicationTests {
-
+    @Test
+    void contextLoads() {
+    }
 
 }
